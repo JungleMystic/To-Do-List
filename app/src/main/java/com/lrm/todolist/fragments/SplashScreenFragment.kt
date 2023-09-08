000facd0cdbbabf421cede1a88adafaaa43299ff
JungleMystic <locharla.rammohan@gmail.com>
2023-09-08 09:00:42 +0530

    Initial Commit v1.0.0

---
+++ app/src/main/java/com/lrm/todolist/fragments/SplashScreenFragment.kt
@@ -0,0 +1,12 @@
+package com.lrm.todolist.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lrm.todolist.R
+
+class SplashScreenFragment : Fragment() {
+
+}
